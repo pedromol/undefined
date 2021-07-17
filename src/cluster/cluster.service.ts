c293bebbf8dc346fe9191933e7134eb88cbdbef9
Pedro Mol <pedro.mol@bemobi.com.br>
2021-07-17 08:00:48 -0300

    fix/add context to cluster service log;

--- src/cluster/cluster.service.ts
+++ src/cluster/cluster.service.ts
@@ -7,18 +7,18 @@ import StaticLogger from '../logger/logger.static';
 export class ClusterService {
   static createCluster(callback: { (): Promise<void>; (): void }): void {
     if ((cluster as unknown as cluster.Cluster).isMaster) {
-      StaticLogger.getLogger().log(`Master server started on ${process.pid}`);
+      StaticLogger.getLogger().log(`Master server started on ${process.pid}`, 'ClusterService');
 
       for (let i = 0; i < os.cpus().length; i++) {
         (cluster as unknown as cluster.Cluster).fork();
       }
 
       (cluster as unknown as cluster.Cluster).on('exit', (worker: { process: { pid: number } }): void => {
-        StaticLogger.getLogger().log(`Worker server ${worker.process.pid} died. Restarting`);
+        StaticLogger.getLogger().log(`Worker server ${worker.process.pid} died. Restarting`, 'ClusterService');
         (cluster as unknown as cluster.Cluster).fork();
       });
     } else {
-      StaticLogger.getLogger().log(`Worker server started on ${process.pid}`);
+      StaticLogger.getLogger().log(`Worker server started on ${process.pid}`, 'ClusterService');
       callback();
     }
   }
